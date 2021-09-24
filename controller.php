3a989287b239e0cd07ff57976d9b559501ed9f29
Ryan Hewitt <ryan@mesuva.com.au>
2021-09-24 17:17:40 +0100

    Update for v9

--- controller.php
+++ controller.php
@@ -9,12 +9,12 @@ class Controller extends Package {
 
 	protected $pkgHandle = 'msv_script_output';
 	protected $appVersionRequired = '5.7.5';
-	protected $pkgVersion = '1.0';
-	
+	protected $pkgVersion = '1.1';
+
 	public function getPackageDescription() {
 		return t("A block to control the output of ad-hoc scripts");
 	}
-	
+
 	public function getPackageName() {
 		return t("Script Output");
 	}
@@ -22,7 +22,7 @@ class Controller extends Package {
 	public function upgrade() {
 		parent::upgrade();
 	}
-	 
+
 	public function install() {
 	   $pkg = parent::install();
 
